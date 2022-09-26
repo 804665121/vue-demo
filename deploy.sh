83ac0516ec117138cd72287a2de35fa8fe3a8969
liulu <804665121@qq.com>
2022-09-26 23:38:55 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+git push -f https://github.com/804665121/vue-demo.git master:gh-pages
 cd -
